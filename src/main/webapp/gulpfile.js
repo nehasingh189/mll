6e00c781ff4ddd7536b5c720d5478a852b19f444
AlexNarayanan <narayanan.a@husky.neu.edu>
2017-02-06 21:30:00 -0500

    gulpfile improvements

--- src/main/webapp/gulpfile.js
+++ src/main/webapp/gulpfile.js
@@ -1,56 +1,73 @@
 'use strict';
 
-let gulp = require('gulp');
-
-let jshint = require('gulp-jshint');
-let stylish = require('jshint-stylish');
-
 let del = require('del');
-
-let templateCache = require('gulp-angular-templatecache');
+let gulp = require('gulp');
 let htmlmin = require('gulp-htmlmin');
-
-let usemin = require('gulp-usemin');
-let uglify = require('gulp-uglify');
+let jshint = require('gulp-jshint');
 let minifycss = require('gulp-minify-css');
 let rev = require('gulp-rev');
-
+let runSequence = require('run-sequence');
 let server = require('karma').Server;
+let stylish = require('jshint-stylish');
+let templateCache = require('gulp-angular-templatecache');
+let uglify = require('gulp-uglify');
+let usemin = require('gulp-usemin');
 
-/*
-    Code Quality Task
-*/
-gulp.task('jshint', () => {
-   gulp.src(['./source/scripts/**/*.js', '!./source/scripts/modules/templates/*.js'])
-       .pipe(jshint())
-       .pipe(jshint.reporter(stylish));
+/**
+ * Build Task
+ *      Cleans the dist and rebuilds files
+ */
+gulp.task('build', (done) => {
+    runSequence('cleanup', ['fonts', 'templates'], 'usemin', function() {
+        done();
+    })
 });
 
-/*
-    Clean-Up Task
-*/
-gulp.task('cleanup', () => del(['./dist', './index.html']));
-
-/*
-    Usemin Task
-*/
-gulp.task('usemin', ['templates'], function () {
-    return gulp.src('./source/index.html')
-        .pipe(usemin({
-            cssBootstrap: [minifycss(), rev()],
-            cssCustom: [minifycss(), rev()],
-            jsJQuery: [/*uglify(), */rev()],
-            jsBootstrap: [/*uglify(), */rev()],
-            jsAngular: [/*uglify(), */rev()],
-            jsTemplates: [/*uglify(), */rev()],
-            jsCustom: [rev()]
-        }))
-        .pipe(gulp.dest('./'));
+/**
+ * Clean-Up Task
+ *      Deletes the dist directory and the generated index file
+ */
+gulp.task('cleanup', (done) => {
+    del(['./dist', './index.html']);
+    done();
 });
 
-/*
-    Template-Cache Task
-*/
+/**
+ * Develop Task
+ *      Automatically watches files and rebuilds if there is a change
+ */
+gulp.task('develop', (done) => {
+    gulp.watch('./source/*.*', { interval: 1000 }, ['build']);
+    console.log('Develop is running! Make some changes in ./source/');
+    console.log('CTRL^C to exit');
+});
+
+/**
+ * Fonts Task
+ *      Generates the fonts folder in the dist
+ */
+gulp.task('fonts', () => {
+    gulp.src('./bower_components/font-awesome/fonts/**/*.{ttf,woff,eof,svg}*')
+        .pipe(gulp.dest('./dist/fonts'));
+    gulp.src('./bower_components/bootstrap/dist/fonts/**/*.{ttf,woff,eof,svg}*')
+        .pipe(gulp.dest('./dist/fonts'));
+    gulp.src('./source/fonts/*.*')
+        .pipe(gulp.dest('./dist/fonts'));
+});
+
+/**
+ * Code Quality Task
+ */
+gulp.task('jshint', (done) => {
+    gulp.src(['./source/scripts/**/*.js', '!./source/scripts/modules/templates/*.js'])
+        .pipe(jshint())
+        .pipe(jshint.reporter(stylish));
+    done();
+});
+
+/**
+ * Generate the template-cache file
+ */
 gulp.task('templates', () => {
     return gulp.src('./source/scripts/**/*.html')
     //.pipe(htmlmin({ collapseWhitespace: true }))
@@ -66,31 +83,31 @@ gulp.task('templates', () => {
         .pipe(gulp.dest('./source/scripts/modules/templates/'));
 });
 
-/*
-    Fonts Task
-*/
-gulp.task('fonts', () => {
-    gulp.src('./bower_components/font-awesome/fonts/**/*.{ttf,woff,eof,svg}*')
-        .pipe(gulp.dest('./dist/fonts'));
-    gulp.src('./bower_components/bootstrap/dist/fonts/**/*.{ttf,woff,eof,svg}*')
-        .pipe(gulp.dest('./dist/fonts'));
-    gulp.src('./source/fonts/*.*')
-        .pipe(gulp.dest('./dist/fonts'));
-});
-
-/*
-    Tests Task
-*/
-gulp.task('tests', ['usemin', 'fonts'], function (done) {
+/**
+ * Test-runner Task
+ */
+gulp.task('test', function (done) {
     new server({
         configFile: __dirname + '/karma.conf.js',
         singleRun: true
     }, done).start();
 });
 
-/*
-    Default Task
+/**
+ * Usemin Task
+ *      Runs the usemin library to consolidate all the source files.
+ *      Then generates index files with refenrences to the current build.
  */
-gulp.task('default', ['cleanup', 'jshint'], function() {
-    gulp.start('tests');
+gulp.task('usemin', ['templates'], function () {
+    return gulp.src('./source/index.html')
+        .pipe(usemin({
+            cssBootstrap: [minifycss(), rev()],
+            cssCustom: [minifycss(), rev()],
+            jsJQuery: [/*uglify(), */rev()],
+            jsBootstrap: [/*uglify(), */rev()],
+            jsAngular: [/*uglify(), */rev()],
+            jsTemplates: [/*uglify(), */rev()],
+            jsCustom: [rev()]
+        }))
+        .pipe(gulp.dest('./'));
 });
