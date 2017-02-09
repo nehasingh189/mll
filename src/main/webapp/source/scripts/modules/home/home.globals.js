7192056cdca44e455773c48b38670f7d3d54b272
AlexNarayanan <narayanan.a@husky.neu.edu>
2017-02-09 14:16:38 -0500

    refactor angular globals and add new musician roles

--- src/main/webapp/source/scripts/modules/home/home.globals.js
+++ src/main/webapp/source/scripts/modules/home/home.globals.js
@@ -7,58 +7,102 @@
     let editUrl = '/MLL/UpdateSongMetaDataServlet';
     let addBandUrl = '/MLL/ProfileServlet';
     let getBandDetailsUrl = '/MLL/BandServlet/?bandId=';
-    
 
+    // State of the music upload form
     let musicForms = {
-            currentId: 0,
-            submitFormId: 4,
-            data: [
-            	{ title: 'Recording Information', isActive: true },
-            	{ title: 'Master Owner Information', isActive: false },
-                { title: 'Song Ownership Information', isActive: false },
-                { title: 'Recording Upload', isActive: false },
-                { title: 'Summary', isActive: false }
-            ]
+        currentId: 0,
+        submitFormId: 4,
+        data: [
+            {title: 'Recording Information', isActive: true},
+            {title: 'Master Owner Information', isActive: false},
+            {title: 'Song Ownership Information', isActive: false},
+            {title: 'Recording Upload', isActive: false},
+            {title: 'Summary', isActive: false}
+        ]
     };
     
-    
+
+    // Information relevant to a piece of music
     let musicData = {
-                    fileInformation: { name: '', file: null },
-                    generalInformation: {
-                        title: '',
-                        artists: [
-                            { name: '' }
-                        ],
-                        beatRate: 0,
-                        primaryGenre: '',
-                        secondaryGenre: ''
-                    },
-                    ownershipInformation: {
-                        songwriters: [
-                            { name: '', primaryEmail: '', primaryPhone: '', secondaryPhone: '', contribution:'', ownershipPercent: 0, musicianRole:'' }
-                        ],
-                        copyright: '',
-                        pubCompany: '',
-                        pro: ''
-                    },
-                    soundInformation: {
-                        soundOwners: [
-                            { name: '', primaryEmail: '', primaryPhone: '', secondaryPhone: '', contribution:'', ownershipPercent: 0, musicianRole:'' }
-                        ]
-                    },
-                    serverInformation: { }
-                };
-    
-    let musicContributions = ['Lyrics', 'Music', 'Lyrics and Music'].sort();
-    let bandData = { ownershipInformation: { 
-    		bandName:'',
-    		songwriters: [{ 
-    			name: '', primaryEmail: '', primaryPhone: '', secondaryPhone: '', contribution:'', ownershipPercent: 0, musicianRole:'' 
-    		}
-        ]}
-};
-    let musicianRoles = ['Vocals', 'Backup Vocals', 'Percussion', 'Guitar', 'Drums', 'Keyboard'].sort();
-    
+        fileInformation: {name: '', file: null},
+        generalInformation: {
+            title: '',
+            artists: [
+                {name: ''}
+            ],
+            beatRate: 0,
+            primaryGenre: '',
+            secondaryGenre: ''
+        },
+        ownershipInformation: {
+            songwriters: [
+                {
+                    name: '',
+                    primaryEmail: '',
+                    primaryPhone: '',
+                    secondaryPhone: '',
+                    contribution: '',
+                    ownershipPercent: 0,
+                    musicianRole: ''
+                }
+            ],
+            copyright: '',
+            pubCompany: '',
+            pro: ''
+        },
+        soundInformation: {
+            soundOwners: [
+                {
+                    name: '',
+                    primaryEmail: '',
+                    primaryPhone: '',
+                    secondaryPhone: '',
+                    contribution: '',
+                    ownershipPercent: 0,
+                    musicianRole: ''
+                }
+            ]
+        },
+        serverInformation: {}
+    };
+
+    // Enumeration of the various types of contributions to a song
+    let musicContributions = [
+        'Lyrics',
+        'Music',
+        'Lyrics and Music'
+    ];
+
+    // The master contact information for a single band
+    let bandData = {
+        ownershipInformation: {
+            bandName: '',
+            songwriters: [{
+                name: '',
+                primaryEmail: '',
+                primaryPhone: '',
+                secondaryPhone: '',
+                contribution: '',
+                ownershipPercent: 0,
+                musicianRole: ''
+            }]
+        }
+    };
+
+    // Enumeration of the different roles/positions in a band
+    let musicianRoles = [
+        'Vocals',
+        'Guitar',
+        'Bass',
+        'Drums',
+        'Keyboard',
+        'Backup Vocals',
+        'Percussion',
+        'Brass',
+        'Woodwinds',
+        'Strings'
+    ];
+
     angular
         .module('mllApp.home')
         .constant('musicianUrl', musicianUrl)
