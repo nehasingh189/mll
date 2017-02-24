e052a20234685daf76759eb56eb681fde76a1c28
isignisi <guo.wi@husky.neu.edu>
2017-02-24 14:14:05 -0500

    Style change on files | Updated gitignore

--- src/main/java/mll/service/PlaylistReferenceService.java
+++ src/main/java/mll/service/PlaylistReferenceService.java
@@ -19,85 +19,78 @@ import mll.dao.PlaylistReferenceDAO;
 
 public class PlaylistReferenceService {
 
-	/*
+  /*
 	 * This method takes in the request object containing the type of request i.e. add/delete/get
-	 * and constructs the response object with the updated playlist.
-	 * Author: Vishal Sanjiv Kotak
-	 * Date: 11/26/2016
-	 */
-	@SuppressWarnings({ "unchecked", "null", "unused" })
-	public JSONObject handlePlaylistReferenceRequest(HttpServletRequest request, HttpServletResponse response) {
-		
-		JSONObject responseObject = new JSONObject();
-		if(request.getSession().getAttribute("userId") != null) {
-			int userId = Integer.parseInt(request.getSession().getAttribute("userId").toString());
-			if(request.getParameter("actionType").equals("add")) {
-				String playlistName = request.getParameter("playlistName");
-				boolean isSuccess = addPlaylistForUser(userId, playlistName);
-				if(isSuccess) {
-					List<PlaylistReference> playlists = getAllPlaylistsForUser(userId);
-					JSONArray playlistReferences = convertToJson(playlists);
-					responseObject.put("playlists", playlistReferences);
-					responseObject.put("isValid", true);
-				}
-				else {
-					List<PlaylistReference> playlists = getAllPlaylistsForUser(userId);
-					JSONArray playlistReferences = convertToJson(playlists);
-					responseObject.put("playlists", playlistReferences);
-					responseObject.put("isValid", false);
-				}
-			}
-			else if(request.getParameter("actionType").equals("shared")) {
-				List<PlaylistReference> playlists = getSharedPlaylists();
-				JSONArray playlistReferences = convertToJson(playlists);
-				responseObject.put("playlists", playlistReferences);
-				responseObject.put("isValid", true);
-			}
-			else if(request.getParameter("actionType").equals("get")) {
-				List<PlaylistReference> playlists = getAllPlaylistsForUser(userId);
-				JSONArray playlistReferences = convertToJson(playlists);
-				responseObject.put("playlists", playlistReferences);
-				responseObject.put("isValid", true);
-			}
-			else if(request.getParameter("actionType").equals("delete")){
-				int playlistId = Integer.parseInt(request.getParameter("playlistId"));
-				boolean flag = deletePlaylistForUser(playlistId, userId);
-				List<PlaylistReference> playlists = getAllPlaylistsForUser(userId);
-				JSONArray playlistReferences = convertToJson(playlists);
-				responseObject.put("playlists", playlistReferences);
-				responseObject.put("isValid", true);
-			}
-			else if(request.getParameter("actionType").equals("addToShare")) {
-				int playlistId = Integer.parseInt(request.getParameter("playlistId"));
-				responseObject = setPlaylistToGlobal(userId, playlistId);
-			}
-			else if(request.getParameter("actionType").equals("unShare")) 
-			{
-				int playlistId = Integer.parseInt(request.getParameter("playlistId"));
-				responseObject = removeFromShare(userId, playlistId);
-			}
+   * and constructs the response object with the updated playlist.
+   * Author: Vishal Sanjiv Kotak
+   * Date: 11/26/2016
+   */
+  @SuppressWarnings({"unchecked", "null", "unused"})
+  public JSONObject handlePlaylistReferenceRequest(HttpServletRequest request,
+      HttpServletResponse response) {
 
-		}
-		else {
-			responseObject.put("isValid", false);
-			responseObject.put("playlists", null);
-		}
-		return responseObject;
-	}
-	
-	
-	public boolean deletePlaylistForUser(int playlistId, int userId) {
-		
-		boolean flag = new PlaylistReferenceDAO().deletePlaylist(playlistId, userId);
-		return flag;
-		
-	}
-	
-	public List<PlaylistReference> getSharedPlaylists() {
+    JSONObject responseObject = new JSONObject();
+    if (request.getSession().getAttribute("userId") != null) {
+      int userId = Integer.parseInt(request.getSession().getAttribute("userId").toString());
+      if (request.getParameter("actionType").equals("add")) {
+        String playlistName = request.getParameter("playlistName");
+        boolean isSuccess = addPlaylistForUser(userId, playlistName);
+        if (isSuccess) {
+          List<PlaylistReference> playlists = getAllPlaylistsForUser(userId);
+          JSONArray playlistReferences = convertToJson(playlists);
+          responseObject.put("playlists", playlistReferences);
+          responseObject.put("isValid", true);
+        } else {
+          List<PlaylistReference> playlists = getAllPlaylistsForUser(userId);
+          JSONArray playlistReferences = convertToJson(playlists);
+          responseObject.put("playlists", playlistReferences);
+          responseObject.put("isValid", false);
+        }
+      } else if (request.getParameter("actionType").equals("shared")) {
+        List<PlaylistReference> playlists = getSharedPlaylists();
+        JSONArray playlistReferences = convertToJson(playlists);
+        responseObject.put("playlists", playlistReferences);
+        responseObject.put("isValid", true);
+      } else if (request.getParameter("actionType").equals("get")) {
+        List<PlaylistReference> playlists = getAllPlaylistsForUser(userId);
+        JSONArray playlistReferences = convertToJson(playlists);
+        responseObject.put("playlists", playlistReferences);
+        responseObject.put("isValid", true);
+      } else if (request.getParameter("actionType").equals("delete")) {
+        int playlistId = Integer.parseInt(request.getParameter("playlistId"));
+        boolean flag = deletePlaylistForUser(playlistId, userId);
+        List<PlaylistReference> playlists = getAllPlaylistsForUser(userId);
+        JSONArray playlistReferences = convertToJson(playlists);
+        responseObject.put("playlists", playlistReferences);
+        responseObject.put("isValid", true);
+      } else if (request.getParameter("actionType").equals("addToShare")) {
+        int playlistId = Integer.parseInt(request.getParameter("playlistId"));
+        responseObject = setPlaylistToGlobal(userId, playlistId);
+      } else if (request.getParameter("actionType").equals("unShare")) {
+        int playlistId = Integer.parseInt(request.getParameter("playlistId"));
+        responseObject = removeFromShare(userId, playlistId);
+      }
 
-		boolean shared = true;
-		return new PlaylistReferenceDAO().getSharedPlaylists(shared);
-	}
+    } else {
+      responseObject.put("isValid", false);
+      responseObject.put("playlists", null);
+    }
+    return responseObject;
+  }
+
+
+  public boolean deletePlaylistForUser(int playlistId, int userId) {
+
+    boolean flag = new PlaylistReferenceDAO().deletePlaylist(playlistId, userId);
+    return flag;
+
+  }
+
+  public List<PlaylistReference> getSharedPlaylists() {
+
+    boolean shared = true;
+    return new PlaylistReferenceDAO().getSharedPlaylists(shared);
+  }
 	
 	/*
 	 * This method takes in the user identifier and the playlist name and adds a 
@@ -105,23 +98,24 @@ public class PlaylistReferenceService {
 	 * Author: Vishal Sanjiv Kotak
 	 * Date: 11/26/2016
 	 */
-	
-	
-	public boolean addPlaylistForUser(int userId, String playlistName) {
-		PlaylistReferenceDAO playlistReferenceDAO = new PlaylistReferenceDAO();
-		PlaylistReference playlistReference = new PlaylistReference();
-		playlistReference.setId(0);
-		playlistReference.setPlaylistName(playlistName);
-		playlistReference.setUserId(userId);
-		String userName = new ArUserDAO().getUserName(userId);
-		if(userName == null)
-			return false;
-		playlistReference.setUserName(userName);
-		playlistReference.setIsShared(false);
-		playlistReference.setCreationDate(new Date());
-		boolean flag = playlistReferenceDAO.addPlaylist(playlistReference);
-		return flag;
-	}
+
+
+  public boolean addPlaylistForUser(int userId, String playlistName) {
+    PlaylistReferenceDAO playlistReferenceDAO = new PlaylistReferenceDAO();
+    PlaylistReference playlistReference = new PlaylistReference();
+    playlistReference.setId(0);
+    playlistReference.setPlaylistName(playlistName);
+    playlistReference.setUserId(userId);
+    String userName = new ArUserDAO().getUserName(userId);
+    if (userName == null) {
+      return false;
+    }
+    playlistReference.setUserName(userName);
+    playlistReference.setIsShared(false);
+    playlistReference.setCreationDate(new Date());
+    boolean flag = playlistReferenceDAO.addPlaylist(playlistReference);
+    return flag;
+  }
 
 	
 	/*
@@ -130,13 +124,12 @@ public class PlaylistReferenceService {
 	 * Author: Vishal Sanjiv Kotak
 	 * Date: 11/26/2016
 	 */
-	
-	
-	
-	public List<PlaylistReference> getAllPlaylistsForUser(int userId) {
 
-		return new PlaylistReferenceDAO().getAllPlaylistsForUserId(userId);
-	}
+
+  public List<PlaylistReference> getAllPlaylistsForUser(int userId) {
+
+    return new PlaylistReferenceDAO().getAllPlaylistsForUserId(userId);
+  }
 	
 	/*
 	 * This method takes in the playlists and returns a JSON Array
@@ -145,45 +138,46 @@ public class PlaylistReferenceService {
 	 * Date: 11/26/2016
 	 * 
 	 */
-	
-	
-	@SuppressWarnings("unchecked")
-	public JSONArray convertToJson(List<PlaylistReference> playlistReference) {
-		JSONArray jsonArrayPlaylist = new JSONArray();
-		for(int i = 0; i< playlistReference.size(); i++){
-			JSONObject object = new JSONObject();
-			object.put("id", playlistReference.get(i).getId());
-			object.put("userId", playlistReference.get(i).getUserId());
-			object.put("playlistName", playlistReference.get(i).getPlaylistName());
-			object.put("isShared", playlistReference.get(i).getIsShared());
-			DateFormat format = new SimpleDateFormat("dd-mm-yyyy");
-			object.put("creationDate", (playlistReference.get(i).getCreationDate().getMonth() + 1) + "-" + 
-						playlistReference.get(i).getCreationDate().getDate() + "-" + (playlistReference.get(i).getCreationDate().getYear() + 1900));
-			object.put("userName", playlistReference.get(i).getUserName());
-			jsonArrayPlaylist.add(object);			
-		}
-		return jsonArrayPlaylist;
-	}
-	
-	
-	@SuppressWarnings("unchecked")
-	public JSONObject setPlaylistToGlobal(int userId, int playlistId) {
 
-		boolean isPlaylistUpdated = new PlaylistReferenceDAO().setPlaylistToGlobal(userId, playlistId);
-		List<PlaylistReference> playlists = getAllPlaylistsForUser(userId);
-		JSONArray playlistReferences = convertToJson(playlists);
-		JSONObject responseObject = new JSONObject();
-		responseObject.put("playlists", playlistReferences);
-		responseObject.put("isValid", isPlaylistUpdated);
-		return responseObject;
-	}
-		
-	public JSONObject removeFromShare(int userId, int playlistId) {
-		JSONObject responseObject = new JSONObject();
-		boolean success = new PlaylistReferenceDAO().removePlaylistFromGlobal(userId,playlistId);
-		responseObject.put("success", success);
-		return responseObject;
-	}
-	
-	
+
+  @SuppressWarnings("unchecked")
+  public JSONArray convertToJson(List<PlaylistReference> playlistReference) {
+    JSONArray jsonArrayPlaylist = new JSONArray();
+    for (int i = 0; i < playlistReference.size(); i++) {
+      JSONObject object = new JSONObject();
+      object.put("id", playlistReference.get(i).getId());
+      object.put("userId", playlistReference.get(i).getUserId());
+      object.put("playlistName", playlistReference.get(i).getPlaylistName());
+      object.put("isShared", playlistReference.get(i).getIsShared());
+      DateFormat format = new SimpleDateFormat("dd-mm-yyyy");
+      object.put("creationDate", (playlistReference.get(i).getCreationDate().getMonth() + 1) + "-" +
+          playlistReference.get(i).getCreationDate().getDate() + "-" + (
+          playlistReference.get(i).getCreationDate().getYear() + 1900));
+      object.put("userName", playlistReference.get(i).getUserName());
+      jsonArrayPlaylist.add(object);
+    }
+    return jsonArrayPlaylist;
+  }
+
+
+  @SuppressWarnings("unchecked")
+  public JSONObject setPlaylistToGlobal(int userId, int playlistId) {
+
+    boolean isPlaylistUpdated = new PlaylistReferenceDAO().setPlaylistToGlobal(userId, playlistId);
+    List<PlaylistReference> playlists = getAllPlaylistsForUser(userId);
+    JSONArray playlistReferences = convertToJson(playlists);
+    JSONObject responseObject = new JSONObject();
+    responseObject.put("playlists", playlistReferences);
+    responseObject.put("isValid", isPlaylistUpdated);
+    return responseObject;
+  }
+
+  public JSONObject removeFromShare(int userId, int playlistId) {
+    JSONObject responseObject = new JSONObject();
+    boolean success = new PlaylistReferenceDAO().removePlaylistFromGlobal(userId, playlistId);
+    responseObject.put("success", success);
+    return responseObject;
+  }
+
+
 }
--- src/test/java/mll/service/PlaylistReferenceServiceTest.java
+++ src/test/java/mll/service/PlaylistReferenceServiceTest.java
@@ -13,279 +13,232 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.junit.Test;
 import mll.beans.PlaylistReference;
+
 import static org.mockito.Mockito.*;
 
 public class PlaylistReferenceServiceTest {
-		
-	@Test
-	public void testGetAllPlaylistsForUserId1()
-	{
-		try 
-		{
-			PlaylistReferenceService service = new PlaylistReferenceService();
-			assertEquals(true, service.getAllPlaylistsForUser(-1) == null);
-		} 
-		catch (Exception e) 
-		{
-		}
-	}
-	
-	@SuppressWarnings("unchecked")
-	@Test
-	public void testConvertToJson1() 
-	{
-		try 
-		{
-			PlaylistReferenceService service = new PlaylistReferenceService();
-			List<PlaylistReference> playlists = new ArrayList<PlaylistReference>();
-			PlaylistReference playlistReference = new PlaylistReference();
-			playlistReference.setId(1);
-			playlistReference.setPlaylistName("Test");
-			playlistReference.setUserId(1);
-			playlistReference.setCreationDate(new Date());
-			playlistReference.setIsShared(false);
-			playlists.add(playlistReference);
-			JSONArray jsonArray = new JSONArray();
-			JSONObject object = new JSONObject();
-			object.put("id", 1);
-			object.put("playlistName", "Test");
-			object.put("userId", 1);
-			jsonArray.add(object);
-			int count = service.convertToJson(playlists).size();
-			assertEquals(true, count==jsonArray.size());
-		}
-		catch(Exception e)
-		{
-			
-		}
-	}
-	
-	
-	@Test
-	public void testSetPlaylistToGlobal1() 
-	{
-		try 
-		{
-			int userId = 0;
-			int playlistId = 0;
-			assertEquals(true, new PlaylistReferenceService().setPlaylistToGlobal(userId, playlistId) != null);
-			
-		} catch (Exception e) 
-		{
-		
-		}
-	}
-	
-	@Test
-	public void testSetPlaylistToGlobal2() 
-	{
-		try 
-		{
-			int userId = 0;
-			int playlistId = 10;
-			assertEquals(true, new PlaylistReferenceService().setPlaylistToGlobal(userId, playlistId) != null);
-			
-		} catch (Exception e) 
-		{
-		
-		}
-	}
-	
-	@Test
-	public void testAddPlaylistForUser2() 
-	{
-		try 
-		{
-			int userId = 1;
-			String playlistName = null;
-			assertEquals(true, new PlaylistReferenceService().addPlaylistForUser(userId, playlistName) != true);
-			
-		} catch (Exception e) 
-		{
-		
-		}
-	}
-	
-	@Test
-	public void testGetSharedPlaylist1()
-	{
-		try 
-		{
-			assertEquals(true, new PlaylistReferenceService().getSharedPlaylists() != null);		
-		}
-		catch(Exception e)
-		{
-			
-		}
-	}
-	
-	@Test 
-	public void testDeletePlaylistForUser1()
-	{
-		try 
-		{
-			
-			int playlistId = 100000;
-			int userId = 100000;			
-			assertEquals(false, new PlaylistReferenceService().deletePlaylistForUser(playlistId, userId));
-		}
-		catch(Exception e)
-		{
-			
-		}
-	}
-	
-	@Test 
-	public void testDeletePlaylistForUser2()
-	{
-		try 
-		{
-			int playlistId = -1;
-			int userId = -1;
-			assertEquals(false, new PlaylistReferenceService().deletePlaylistForUser(playlistId, userId));
-		}
-		catch(Exception e)
-		{
-			
-		}
-	}
-	
-	@Test
-	public void testRemoveFromShare()
-	{
-		try
-		{
-			assertEquals(true, new PlaylistReferenceService().removeFromShare(1, 10000)!=null);
-		}
-		catch(Exception e)
-		{
 
-		}
+  @Test
+  public void testGetAllPlaylistsForUserId1() {
+    try {
+      PlaylistReferenceService service = new PlaylistReferenceService();
+      assertEquals(true, service.getAllPlaylistsForUser(-1) == null);
+    } catch (Exception e) {
+    }
+  }
 
-	}
-	
-	@Test
-	public void testHandlePlaylistReferenceRequest2()
-	{
-		try
-		{
-			HttpServletRequest request = mock(HttpServletRequest.class);
-			HttpServletResponse response = mock(HttpServletResponse.class);
-			HttpSession session = mock(HttpSession.class);
-			when(session.getAttribute("userId")).thenReturn("3"); 
-			when(request.getSession()).thenReturn(session);
-			when(request.getParameter("actionType")).thenReturn("add");
-			when(request.getParameter("playlistName")).thenReturn("mlltest1");
-			
-			JSONObject jsonObject = new PlaylistReferenceService().handlePlaylistReferenceRequest(request, response);
-			assertEquals(true, jsonObject!=null);
-		}
-		catch(Exception e)
-		{
-			
-		}
-	}
-	
-	@Test
-	public void testHandlePlaylistReferenceRequest3()
-	{
-		try
-		{
-			HttpServletRequest request = mock(HttpServletRequest.class);
-			HttpServletResponse response = mock(HttpServletResponse.class);
-			HttpSession session = mock(HttpSession.class);
-			when(session.getAttribute("userId")).thenReturn("3"); 
-			when(request.getSession()).thenReturn(session);
-			when(request.getParameter("actionType")).thenReturn("shared");
-			JSONObject jsonObject = new PlaylistReferenceService().handlePlaylistReferenceRequest(request, response);
-			assertEquals(true, jsonObject!=null);
-		}
-		catch(Exception e)
-		{
-			
-		}
-	}
-	
-	@Test
-	public void testHandlePlaylistReferenceRequest4()
-	{
-		try
-		{
-			HttpServletRequest request = mock(HttpServletRequest.class);
-			HttpServletResponse response = mock(HttpServletResponse.class);
-			HttpSession session = mock(HttpSession.class);
-			when(session.getAttribute("userId")).thenReturn("3"); 
-			when(request.getSession()).thenReturn(session);
-			when(request.getParameter("actionType")).thenReturn("get");
-			JSONObject jsonObject = new PlaylistReferenceService().handlePlaylistReferenceRequest(request, response);
-			assertEquals(true, jsonObject!=null);
-		}
-		catch(Exception e)
-		{
-			
-		}
-	}
-	
-	@Test
-	public void testHandlePlaylistReferenceRequest5()
-	{
-		try
-		{
-			HttpServletRequest request = mock(HttpServletRequest.class);
-			HttpServletResponse response = mock(HttpServletResponse.class);
-			HttpSession session = mock(HttpSession.class);
-			when(session.getAttribute("userId")).thenReturn("3"); 
-			when(request.getSession()).thenReturn(session);
-			when(request.getParameter("playlistId")).thenReturn("100000");
-			when(request.getParameter("actionType")).thenReturn("delete");
-			JSONObject jsonObject = new PlaylistReferenceService().handlePlaylistReferenceRequest(request, response);
-			assertEquals(true, jsonObject!=null);
-		}
-		catch(Exception e)
-		{
-			
-		}
-	}
-	
-	@Test
-	public void testHandlePlaylistReferenceRequest6()
-	{
-		try
-		{
-			HttpServletRequest request = mock(HttpServletRequest.class);
-			HttpServletResponse response = mock(HttpServletResponse.class);
-			HttpSession session = mock(HttpSession.class);
-			when(session.getAttribute("userId")).thenReturn("3"); 
-			when(request.getSession()).thenReturn(session);
-			when(request.getParameter("actionType")).thenReturn("addToShare");
-			JSONObject jsonObject = new PlaylistReferenceService().handlePlaylistReferenceRequest(request, response);
-			assertEquals(true, jsonObject!=null);
-		}
-		catch(Exception e)
-		{
-			
-		}
-	}
-	
-	@Test
-	public void testHandlePlaylistReferenceRequest7()
-	{
-		try
-		{
-			HttpServletRequest request = mock(HttpServletRequest.class);
-			HttpServletResponse response = mock(HttpServletResponse.class);
-			HttpSession session = mock(HttpSession.class);
-			when(session.getAttribute("userId")).thenReturn("3"); 
-			when(request.getSession()).thenReturn(session);
-			when(request.getParameter("actionType")).thenReturn("unShare");
-			when(request.getParameter("playlistId")).thenReturn("10000");
-			JSONObject jsonObject = new PlaylistReferenceService().handlePlaylistReferenceRequest(request, response);
-			assertEquals(true, jsonObject!=null);
-		}
-		catch(Exception e)
-		{
-			
-		}
-	}
-	
+  @SuppressWarnings("unchecked")
+  @Test
+  public void testConvertToJson1() {
+    try {
+      PlaylistReferenceService service = new PlaylistReferenceService();
+      List<PlaylistReference> playlists = new ArrayList<PlaylistReference>();
+      PlaylistReference playlistReference = new PlaylistReference();
+      playlistReference.setId(1);
+      playlistReference.setPlaylistName("Test");
+      playlistReference.setUserId(1);
+      playlistReference.setCreationDate(new Date());
+      playlistReference.setIsShared(false);
+      playlists.add(playlistReference);
+      JSONArray jsonArray = new JSONArray();
+      JSONObject object = new JSONObject();
+      object.put("id", 1);
+      object.put("playlistName", "Test");
+      object.put("userId", 1);
+      jsonArray.add(object);
+      int count = service.convertToJson(playlists).size();
+      assertEquals(true, count == jsonArray.size());
+    } catch (Exception e) {
+
+    }
+  }
+
+
+  @Test
+  public void testSetPlaylistToGlobal1() {
+    try {
+      int userId = 0;
+      int playlistId = 0;
+      assertEquals(true,
+          new PlaylistReferenceService().setPlaylistToGlobal(userId, playlistId) != null);
+
+    } catch (Exception e) {
+
+    }
+  }
+
+  @Test
+  public void testSetPlaylistToGlobal2() {
+    try {
+      int userId = 0;
+      int playlistId = 10;
+      assertEquals(true,
+          new PlaylistReferenceService().setPlaylistToGlobal(userId, playlistId) != null);
+
+    } catch (Exception e) {
+
+    }
+  }
+
+  @Test
+  public void testAddPlaylistForUser2() {
+    try {
+      int userId = 1;
+      String playlistName = null;
+      assertEquals(true,
+          new PlaylistReferenceService().addPlaylistForUser(userId, playlistName) != true);
+
+    } catch (Exception e) {
+
+    }
+  }
+
+  @Test
+  public void testGetSharedPlaylist1() {
+    try {
+      assertEquals(true, new PlaylistReferenceService().getSharedPlaylists() != null);
+    } catch (Exception e) {
+
+    }
+  }
+
+  @Test
+  public void testDeletePlaylistForUser1() {
+    try {
+
+      int playlistId = 100000;
+      int userId = 100000;
+      assertEquals(false, new PlaylistReferenceService().deletePlaylistForUser(playlistId, userId));
+    } catch (Exception e) {
+
+    }
+  }
+
+  @Test
+  public void testDeletePlaylistForUser2() {
+    try {
+      int playlistId = -1;
+      int userId = -1;
+      assertEquals(false, new PlaylistReferenceService().deletePlaylistForUser(playlistId, userId));
+    } catch (Exception e) {
+
+    }
+  }
+
+  @Test
+  public void testRemoveFromShare() {
+    try {
+      assertEquals(true, new PlaylistReferenceService().removeFromShare(1, 10000) != null);
+    } catch (Exception e) {
+
+    }
+
+  }
+
+  @Test
+  public void testHandlePlaylistReferenceRequest2() {
+    try {
+      HttpServletRequest request = mock(HttpServletRequest.class);
+      HttpServletResponse response = mock(HttpServletResponse.class);
+      HttpSession session = mock(HttpSession.class);
+      when(session.getAttribute("userId")).thenReturn("3");
+      when(request.getSession()).thenReturn(session);
+      when(request.getParameter("actionType")).thenReturn("add");
+      when(request.getParameter("playlistName")).thenReturn("mlltest1");
+
+      JSONObject jsonObject = new PlaylistReferenceService()
+          .handlePlaylistReferenceRequest(request, response);
+      assertEquals(true, jsonObject != null);
+    } catch (Exception e) {
+
+    }
+  }
+
+  @Test
+  public void testHandlePlaylistReferenceRequest3() {
+    try {
+      HttpServletRequest request = mock(HttpServletRequest.class);
+      HttpServletResponse response = mock(HttpServletResponse.class);
+      HttpSession session = mock(HttpSession.class);
+      when(session.getAttribute("userId")).thenReturn("3");
+      when(request.getSession()).thenReturn(session);
+      when(request.getParameter("actionType")).thenReturn("shared");
+      JSONObject jsonObject = new PlaylistReferenceService()
+          .handlePlaylistReferenceRequest(request, response);
+      assertEquals(true, jsonObject != null);
+    } catch (Exception e) {
+
+    }
+  }
+
+  @Test
+  public void testHandlePlaylistReferenceRequest4() {
+    try {
+      HttpServletRequest request = mock(HttpServletRequest.class);
+      HttpServletResponse response = mock(HttpServletResponse.class);
+      HttpSession session = mock(HttpSession.class);
+      when(session.getAttribute("userId")).thenReturn("3");
+      when(request.getSession()).thenReturn(session);
+      when(request.getParameter("actionType")).thenReturn("get");
+      JSONObject jsonObject = new PlaylistReferenceService()
+          .handlePlaylistReferenceRequest(request, response);
+      assertEquals(true, jsonObject != null);
+    } catch (Exception e) {
+
+    }
+  }
+
+  @Test
+  public void testHandlePlaylistReferenceRequest5() {
+    try {
+      HttpServletRequest request = mock(HttpServletRequest.class);
+      HttpServletResponse response = mock(HttpServletResponse.class);
+      HttpSession session = mock(HttpSession.class);
+      when(session.getAttribute("userId")).thenReturn("3");
+      when(request.getSession()).thenReturn(session);
+      when(request.getParameter("playlistId")).thenReturn("100000");
+      when(request.getParameter("actionType")).thenReturn("delete");
+      JSONObject jsonObject = new PlaylistReferenceService()
+          .handlePlaylistReferenceRequest(request, response);
+      assertEquals(true, jsonObject != null);
+    } catch (Exception e) {
+
+    }
+  }
+
+  @Test
+  public void testHandlePlaylistReferenceRequest6() {
+    try {
+      HttpServletRequest request = mock(HttpServletRequest.class);
+      HttpServletResponse response = mock(HttpServletResponse.class);
+      HttpSession session = mock(HttpSession.class);
+      when(session.getAttribute("userId")).thenReturn("3");
+      when(request.getSession()).thenReturn(session);
+      when(request.getParameter("actionType")).thenReturn("addToShare");
+      JSONObject jsonObject = new PlaylistReferenceService()
+          .handlePlaylistReferenceRequest(request, response);
+      assertEquals(true, jsonObject != null);
+    } catch (Exception e) {
+
+    }
+  }
+
+  @Test
+  public void testHandlePlaylistReferenceRequest7() {
+    try {
+      HttpServletRequest request = mock(HttpServletRequest.class);
+      HttpServletResponse response = mock(HttpServletResponse.class);
+      HttpSession session = mock(HttpSession.class);
+      when(session.getAttribute("userId")).thenReturn("3");
+      when(request.getSession()).thenReturn(session);
+      when(request.getParameter("actionType")).thenReturn("unShare");
+      when(request.getParameter("playlistId")).thenReturn("10000");
+      JSONObject jsonObject = new PlaylistReferenceService()
+          .handlePlaylistReferenceRequest(request, response);
+      assertEquals(true, jsonObject != null);
+    } catch (Exception e) {
+
+    }
+  }
+
 }
