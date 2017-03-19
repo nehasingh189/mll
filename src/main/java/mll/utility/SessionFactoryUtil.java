26f43994766c02210c7d1e8671874c79fc3bf2db
tlangs <langsford.t@husky.neu.edu>
2017-03-19 14:26:44 -0400

    Encrypt hibernate password (#13)
    * MLP-236 #time 5h #comment Reinstalled the entire database and set it up securely
    
    * MLP-236 #time 2h #comment Implemented encrypted database password.

--- src/main/java/mll/utility/SessionFactoryUtil.java
+++ src/main/java/mll/utility/SessionFactoryUtil.java
@@ -1,8 +1,13 @@
 package mll.utility;
 
+import com.google.common.io.Files;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
- 
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
+
+import java.io.File;
+import java.nio.charset.StandardCharsets;
+
 public class SessionFactoryUtil 
 {
 	 private static final SessionFactory sessionFactory;
@@ -10,10 +15,17 @@ public class SessionFactoryUtil
 	//We need to initialize this object only one time so static block
 	 static
 	 {
-		 try 
-		 {
+		 try {
+			 // Read the key from file to decrypt password
+			 String key = Files.readFirstLine(new File(System.getenv("JASYPT_LOCATION")), StandardCharsets.UTF_8);
+			 StandardPBEStringEncryptor encryptor = new StandardPBEStringEncryptor();
+			 encryptor.setPassword(key);
+
 			 // Create the SessionFactory from hibernate.cfg.xml
-			 sessionFactory = new Configuration().configure().buildSessionFactory();
+			 Configuration configuration = new Configuration().configure();
+			 String hibernatePassword = configuration.getProperty("hibernate.connection.password");
+			 configuration.setProperty("hibernate.connection.password", encryptor.decrypt(hibernatePassword));
+			 sessionFactory = configuration.buildSessionFactory();
 		 } 
 		 catch (Throwable ex) 
 		 {
